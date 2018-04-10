549d4657c3e55d05f22764d98ada3a13ea523c3f
fukuda190 <37056191+fukuda190@users.noreply.github.com>
2018-04-10 14:48:15 +0900

    ターゲットランダム出現（破壊不可）

---
+++ TargetSpown.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetSpown : MonoBehaviour {
+
+    public GameObject TragetPrefab;
+
+	// Use this for initialization
+	void Start () {
+        
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void RandomSpown()
+    {
+        float x = Random.Range(0.0f, 10.0f);
+        float y = 0;
+        float z = Random.Range(0.0f, 10.0f);
+        GameObject Obj = Instantiate(TragetPrefab, new Vector3(x, y, z), Quaternion.identity) as GameObject;
+    }
+
+    public void Hakai()
+    {
+        GameObject.Destroy(gameObject);
+    }
+}
