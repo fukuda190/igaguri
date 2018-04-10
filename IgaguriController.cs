10f524930ffa6630fe1f98f66356501963ab6ef0
hiyokokkoLLS <hiyokokko65535@gmail.com>
2018-04-10 14:45:40 +0900

    スコアの追加

---
+++ IgaguriController.cs
@@ -0,0 +1,37 @@
+﻿//元のからターゲットor地面に当たったら消滅かつターゲットに当たったらスコア増やすコード追加
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgaguriController : MonoBehaviour {
+
+    public int score;
+    public GameObject scoreText;
+
+	public void Shoot(Vector3 dir) {
+		GetComponent<Rigidbody>().AddForce(dir);
+	}
+
+	void OnCollisionEnter(Collision other) {
+
+        if (other.gameObject.tag == "target")
+        {
+            GetComponent<ParticleSystem>().Play();
+            scoreText.GetComponent<ScoreManager>().score += score;
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+
+        if (other.gameObject.tag == "Terrain")
+        {
+            Destroy(this.gameObject);
+        }
+	}
+
+	void Start() {
+        scoreText = GameObject.Find("Score");
+        //score = 1;
+		// Shoot(new Vector3(0, 200, 2000));
+	}
+}
---
+++ ScoreManager.cs
@@ -0,0 +1,31 @@
+﻿//"Score"って名前のテキスト作ってアタッチ
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    public int score;
+    int nowScore;
+
+	// Use this for initialization
+	void Start () {
+
+        score = 0;
+        nowScore = score;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (score != nowScore)
+        {
+            GetComponent<Text>().text = "Score : " + score;
+            nowScore = score;
+        }
+
+	}
+}
