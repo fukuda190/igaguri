bedb5f7ae0b541931f5a5fd60b77e6955e66d2b5
hiyokokkoLLS <hiyokokko65535@gmail.com>
2018-04-10 14:07:19 +0900

    FirstCommit

---
+++ IgaguriDestroy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgaguriDestroy : MonoBehaviour {
+
+    float time;
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        time += Time.deltaTime;
+
+        if (time >= 5)
+        {
+
+            Destroy(this.gameObject);
+
+        }
+		
+	}
+}
